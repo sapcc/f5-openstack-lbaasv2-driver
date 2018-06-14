d414bfeaaf04e9d85b8ce09058e46cdf0a81b57c
Andrew Karpow <andrew.karpow@sap.com>
2018-06-14 11:08:07 +0200

    queens compatibility

--- f5lbaasdriver/v2/bigip/plugin_rpc.py
+++ f5lbaasdriver/v2/bigip/plugin_rpc.py
@@ -20,7 +20,6 @@ from f5lbaasdriver.v2.bigip import constants_v2 as constants
 from neutron.common import rpc as neutron_rpc
 from neutron.db import agents_db
 from neutron.db.models import agent as agents_model
-from neutron.plugins.common import constants as plugin_constants
 
 from neutron_lbaas.db.loadbalancer import models
 from neutron_lbaas.services.loadbalancer import constants as nlb_constant
@@ -165,7 +164,7 @@ class LBaaSv2PluginCallbacksRPC(object):
                     agent.id
                 )
                 for lb in agent_lbs:
-                    if lb.provisioning_status == plugin_constants.ACTIVE:
+                    if lb.provisioning_status == neutron_const.ACTIVE:
                         loadbalancers.append(
                             {
                                 'agent_host': agent['host'],
@@ -194,8 +193,8 @@ class LBaaSv2PluginCallbacksRPC(object):
                     agent.id
                 )
                 for lb in agent_lbs:
-                    if (lb.provisioning_status != plugin_constants.ACTIVE and
-                            lb.provisioning_status != plugin_constants.ERROR):
+                    if (lb.provisioning_status != neutron_const.ACTIVE and
+                            lb.provisioning_status != neutron_const.ERROR):
                         loadbalancers.append(
                             {
                                 'agent_host': agent['host'],
@@ -224,7 +223,7 @@ class LBaaSv2PluginCallbacksRPC(object):
                     agent.id
                 )
                 for lb in agent_lbs:
-                    if (lb.provisioning_status == plugin_constants.ERROR):
+                    if (lb.provisioning_status == neutron_const.ERROR):
                         loadbalancers.append(
                             {
                                 'agent_host': agent['host'],
@@ -261,8 +260,8 @@ class LBaaSv2PluginCallbacksRPC(object):
                     loadbalancer_id
                 )
                 if (lb_db.provisioning_status ==
-                        plugin_constants.PENDING_DELETE):
-                    status = plugin_constants.PENDING_DELETE
+                        neutron_const.PENDING_DELETE):
+                    status = neutron_const.PENDING_DELETE
 
                 self.driver.plugin.db.update_status(
                     context,
@@ -282,7 +281,7 @@ class LBaaSv2PluginCallbacksRPC(object):
 
     @log_helpers.log_method_call
     def update_listener_status(self, context, listener_id=None,
-                               provisioning_status=plugin_constants.ERROR,
+                               provisioning_status=neutron_const.ERROR,
                                operating_status=None):
         """Agent confirmation hook to update listener status."""
         with context.session.begin(subtransactions=True):
@@ -292,8 +291,8 @@ class LBaaSv2PluginCallbacksRPC(object):
                     listener_id
                 )
                 if (listener_db.provisioning_status ==
-                        plugin_constants.PENDING_DELETE):
-                    provisioning_status = plugin_constants.PENDING_DELETE
+                        neutron_const.PENDING_DELETE):
+                    provisioning_status = neutron_const.PENDING_DELETE
                 self.driver.plugin.db.update_status(
                     context,
                     models.Listener,
@@ -312,7 +311,7 @@ class LBaaSv2PluginCallbacksRPC(object):
 
     @log_helpers.log_method_call
     def update_pool_status(self, context, pool_id=None,
-                           provisioning_status=plugin_constants.ERROR,
+                           provisioning_status=neutron_const.ERROR,
                            operating_status=None):
         """Agent confirmations hook to update pool status."""
         with context.session.begin(subtransactions=True):
@@ -322,7 +321,7 @@ class LBaaSv2PluginCallbacksRPC(object):
                     pool_id
                 )
                 if (pool.provisioning_status !=
-                        plugin_constants.PENDING_DELETE):
+                        neutron_const.PENDING_DELETE):
                     self.driver.plugin.db.update_status(
                         context,
                         models.PoolV2,
@@ -351,7 +350,7 @@ class LBaaSv2PluginCallbacksRPC(object):
                     member_id
                 )
                 if (member.provisioning_status !=
-                        plugin_constants.PENDING_DELETE):
+                        neutron_const.PENDING_DELETE):
                     self.driver.plugin.db.update_status(
                         context,
                         models.MemberV2,
@@ -371,7 +370,7 @@ class LBaaSv2PluginCallbacksRPC(object):
     @log_helpers.log_method_call
     def update_health_monitor_status(
             self, context, health_monitor_id,
-            provisioning_status=plugin_constants.ERROR, operating_status=None):
+            provisioning_status=neutron_const.ERROR, operating_status=None):
         """Agent confirmation hook to update health monitor status."""
         with context.session.begin(subtransactions=True):
             try:
@@ -380,7 +379,7 @@ class LBaaSv2PluginCallbacksRPC(object):
                     health_monitor_id
                 )
                 if (health_monitor.provisioning_status !=
-                        plugin_constants.PENDING_DELETE):
+                        neutron_const.PENDING_DELETE):
                     self.driver.plugin.db.update_status(
                         context,
                         models.HealthMonitorV2,
@@ -399,7 +398,7 @@ class LBaaSv2PluginCallbacksRPC(object):
 
     @log_helpers.log_method_call
     def update_l7policy_status(self, context, l7policy_id=None,
-                               provisioning_status=plugin_constants.ERROR,
+                               provisioning_status=neutron_const.ERROR,
                                operating_status=None):
         """Agent confirmation hook to update l7 policy status."""
         with context.session.begin(subtransactions=True):
@@ -409,8 +408,8 @@ class LBaaSv2PluginCallbacksRPC(object):
                     l7policy_id
                 )
                 if (l7policy_db.provisioning_status ==
-                        plugin_constants.PENDING_DELETE):
-                    provisioning_status = plugin_constants.PENDING_DELETE
+                        neutron_const.PENDING_DELETE):
+                    provisioning_status = neutron_const.PENDING_DELETE
                 self.driver.plugin.db.update_status(
                     context,
                     models.L7Policy,
@@ -430,7 +429,7 @@ class LBaaSv2PluginCallbacksRPC(object):
 
     @log_helpers.log_method_call
     def update_l7rule_status(self, context, l7rule_id=None, l7policy_id=None,
-                             provisioning_status=plugin_constants.ERROR,
+                             provisioning_status=neutron_const.ERROR,
                              operating_status=None):
         """Agent confirmation hook to update l7 policy status."""
         with context.session.begin(subtransactions=True):
@@ -441,8 +440,8 @@ class LBaaSv2PluginCallbacksRPC(object):
                     l7policy_id
                 )
                 if (l7rule_db.provisioning_status ==
-                        plugin_constants.PENDING_DELETE):
-                    provisioning_status = plugin_constants.PENDING_DELETE
+                        neutron_const.PENDING_DELETE):
+                    provisioning_status = neutron_const.PENDING_DELETE
                 self.driver.plugin.db.update_status(
                     context,
                     models.L7Rule,
